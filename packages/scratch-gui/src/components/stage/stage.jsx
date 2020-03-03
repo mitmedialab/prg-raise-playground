7c110dc24a6220583b7745fa8f4b2061b376749f
Brian Jordan <bcjordan@gmail.com>
2020-03-03 15:59:14 -0500

    Example overlay blocks

--- packages/scratch-gui/src/components/stage/stage.jsx
+++ packages/scratch-gui/src/components/stage/stage.jsx
@@ -2,6 +2,7 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import classNames from 'classnames';
 
+require("aframe");
 import Box from '../box/box.jsx';
 import DOMElementRenderer from '../../containers/dom-element-renderer.jsx';
 import Loupe from '../loupe/loupe.jsx';
@@ -58,6 +59,22 @@ const StageComponent = props => {
                         width: stageDimensions.width
                     }}
                 >
+                    <a-scene
+                        style={{
+                            position: 'absolute',
+                            right: 0,
+                            top: 0,
+                            height: stageDimensions.height,
+                            width: stageDimensions.width
+                        }}
+                        embedded
+                    >
+                        <a-box position="-1 0.5 -3" rotation="0 45 0" color="#4CC3D9"></a-box>
+                        <a-sphere position="0 1.25 -5" radius="1.25" color="#EF2D5E"></a-sphere>
+                        <a-cylinder position="1 0.75 -3" radius="0.5" height="1.5" color="#FFC65D"></a-cylinder>
+                        <a-plane position="0 0 -4" rotation="-90 0 0" width="4" height="4" color="#7BC8A4"></a-plane>
+                        {/*<a-sky color="#ECECEC"></a-sky>*/}
+                    </a-scene>
                     <DOMElementRenderer
                         domElement={canvas}
                         style={{
@@ -67,6 +84,8 @@ const StageComponent = props => {
                         {...boxProps}
                     />
                 </Box>
+                {/*<Box className={styles.monitorWrapper}>*/}
+                {/*</Box>*/}
                 <Box className={styles.monitorWrapper}>
                     <MonitorList
                         draggable={useEditorDragStyle}
