21b646626ee9505af70fabbcd836fda248078627
Brian Jordan <bcjordan@gmail.com>
2020-04-07 23:30:45 -0400

    Remove comment

--- packages/scratch-vm/src/extensions/scratch3_posenet/index.js
+++ packages/scratch-vm/src/extensions/scratch3_posenet/index.js
@@ -847,45 +847,6 @@ class Scratch3PoseNetBlocks {
         };
     }
 
-    /**
-     smile: 0
-     innerBrowRaise: 0
-     browRaise: 0
-     browFurrow: 0
-     noseWrinkle: 0
-     upperLipRaise: 0
-     lipCornerDepressor: 0
-     chinRaise: 0
-     lipPucker: 0
-     lipPress: 0
-     lipSuck: 0
-     mouthOpen: 0
-     smirk: 0
-     eyeClosure: 0
-     attention: 87.72370910644531
-     lidTighten: 0
-     jawDrop: 0
-     dimpler: 0
-     eyeWiden: 0
-     cheekRaise: 0
-     lipStretch: 0
-     */
-
-    /**
-     *
-     joy: 0
-     sadness: 0
-     disgust: 0
-     contempt: 0
-     anger: 0
-     fear: 0
-     surprise: 0
-     valence: 0
-     engagement: 0
-     * @returns {null|boolean}
-     */
-
-
     affdexMouthOpen() {
         if (!this.affdexState || !this.affdexState.expressions) {
             return null;
