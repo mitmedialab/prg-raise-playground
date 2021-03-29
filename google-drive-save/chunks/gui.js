f2ac75e67cc821cb2986d3c7946fed808488b6a8
randi-c-dubs <randi.w.2012@gmail.com>
2021-03-29 22:40:36 +0000

    Deploying to gh-pages from  0168f8614838509397c6e102f170fda3bbd49313

--- google-drive-save/chunks/gui.js
+++ google-drive-save/chunks/gui.js
@@ -147,7 +147,7 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
 
 
 var onClickLogo = function onClickLogo() {
-  window.location = 'https://' + 'dancingwithai.github.io';
+  window.location = 'https://aieducation.mit.edu';
 };
 
 var handleTelemetryModalCancel = function handleTelemetryModalCancel() {
