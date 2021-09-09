94e2f48cffbe559bac4f3edc317335fa294a352a
randi-c-dubs <randi.w.2012@gmail.com>
2021-09-09 23:14:14 +0000

    Fixed bug, removed intent classifier again

--- packages/scratch-gui/src/containers/blocks.jsx
+++ packages/scratch-gui/src/containers/blocks.jsx
@@ -117,8 +117,6 @@ class Blocks extends React.Component {
         toolboxWorkspace.registerButtonCallback('MAKE_A_VARIABLE', varListButtonCallback(''));
         toolboxWorkspace.registerButtonCallback('MAKE_A_LIST', varListButtonCallback('list'));
         toolboxWorkspace.registerButtonCallback('MAKE_A_PROCEDURE', procButtonCallback);
-        toolboxWorkspace.registerButtonCallback('EDIT_TEXT_MODEL', textModelEditButtonCallback);
-        toolboxWorkspace.registerButtonCallback('EDIT_TEXT_CLASSIFIER', classifierModelEditButtonCallback);
         toolboxWorkspace.registerButtonCallback('CONNECT_MICROBIT_ROBOT', connectMicrobitRobotCallback);
         toolboxWorkspace.registerButtonCallback('CONNECT_DOODLEBOT', connectDoodlebotCallback);
 
