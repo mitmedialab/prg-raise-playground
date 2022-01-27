b713191dd6cb956a834f5c8e7925db912ef24ce6
sghowinem <40252177+sghowinem@users.noreply.github.com>
2022-01-26 19:59:55 -0500

    fix online build with fs

--- packages/scratch-vm/webpack.config.js
+++ packages/scratch-vm/webpack.config.js
@@ -7,6 +7,11 @@ const base = {
     node: {
         fs: 'empty'
     },
+    config.resolve.fallback : {
+        fs: false, 
+        path:false, 
+        "crypto": false
+    },
     mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
     devServer: {
         contentBase: false,
