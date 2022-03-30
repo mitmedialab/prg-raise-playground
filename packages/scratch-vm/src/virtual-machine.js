613e8fe5fb1552b33ab8e6101e9551686ac4f3c3
randi-c-dubs <randi.w.2012@gmail.com>
2022-03-30 21:29:50 +0000

    Removed extra items from package json

--- packages/scratch-vm/src/virtual-machine.js
+++ packages/scratch-vm/src/virtual-machine.js
@@ -33,8 +33,8 @@ const RESERVED_NAMES = ["_mouse_", "_stage_", "_edge_", "_myself_", "_random_"];
 
 const CORE_EXTENSIONS = [
     'textClassification',
-    //'teachableMachine',
-    //'jibo',
+    'teachableMachine',
+    'jibo',
     // 'motion',
     // 'looks',
     // 'sound',
