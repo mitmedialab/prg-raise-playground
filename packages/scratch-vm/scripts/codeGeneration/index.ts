fec41df539a3c59b0cd571249313091335fd1835
p-buddy <pmalacho@usc.edu>
2022-07-19 22:48:31 -0400

    rethinking the design to make things easier

--- packages/scratch-vm/scripts/codeGeneration/index.ts
+++ packages/scratch-vm/scripts/codeGeneration/index.ts
@@ -2,25 +2,29 @@ import { readFileSync } from "fs";
 import path = require("path");
 import { ExtensionMenuDisplayDetails } from "../../src/typescript-support/types";
 
+const relativePathToExtensionDir = ["..", "..", "src", "extensions"];
+const baseExtensionsDir = path.resolve(__dirname, ...relativePathToExtensionDir);
+const getPathToExtension = (extensionId: string) => path.join(baseExtensionsDir, extensionId);
+
 const relativePathToIndexFile = ["..", "..", "..", "scratch-gui", "src", "lib", "libraries", "extensions", "index.jsx"]; 
 const guiIndexFile = path.resolve(__dirname, ...relativePathToIndexFile);
 
 const guardFlagStart = "/* CODE GEN GUARD START: ";
 const guardFlagEnd = "/* CODE GEN GUARD END: ";
 
-const getGuards = (identifier: string) => [`${guardFlagStart} ${identifier}`, `${guardFlagEnd} ${identifier}`, ]
+const getGuards = (identifier: string): [start: string, end: string] => [`${guardFlagStart} ${identifier}`, `${guardFlagEnd} ${identifier}`, ]
 
 const iconImportGuards = getGuards("Icon Import");
 const extensionInfoGuards = getGuards("Extension Info");
 
 export const generateCodeForExtensions = (extensions: Record<string, ExtensionMenuDisplayDetails>) => {
   const guiFileContent = readFileSync(guiIndexFile, {encoding: "utf-8"});
-
   // Remove all lines from guiFileContent within the guards
 
-  // Loop over the entries of extensions to:
-  // 1. Add import lines for the necessary icons within the iconImportGuards. These lines should reference a location within 'packages/scratch-gui/src/extension-gallery-assets'
-  // 2. Add declaration for a minimal minimal menuItem object like the following (NOTE: the key of the entry within the extensions object is to be used as the 'extensionId'):
+  // Loop over the entries of extensions to (NOTE: the key of the entry within the extensions object is to be used as the 'extensionId'):
+  // 1. Look for the specified icon files inside of the given entrie's extension directory, and move these to the location '/packages/scratch-gui/src/extension-gallery-assets/${extensionId}'
+  // 2. Add import lines for the necessary icons within the iconImportGuards. These lines should reference the location setup in step 1. within 'packages/scratch-gui/src/extension-gallery-assets'
+  // 3. Add declaration for a minimal menuItem object like the following:
   /*
     {
         name: "Typescript Realistic Example",
@@ -31,7 +35,7 @@ export const generateCodeForExtensions = (extensions: Record<string, ExtensionMe
         featured: true
     },
   */
- // 3. BONUS! Add suport for additional non-required properties in the ExtensionMenuDisplayDetails type (like 'collaborator', 'featured', 'bluetoothRequired') so that, if they are defined, they are incorporated into the code generation
+ // 4. BONUS! Add suport for additional non-required properties in the ExtensionMenuDisplayDetails type (like 'collaborator', 'featured', 'bluetoothRequired') so that, if they are defined, they are incorporated into the code generation
 
  // Once you've update the files contents, write it back out to the guiIndexFile location
 }
