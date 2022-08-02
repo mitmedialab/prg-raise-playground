4eb2d24d44780ff5c776c8ab0dd53700f470c5c6
p-buddy <pmalacho@usc.edu>
2022-08-02 17:52:53 -0400

    Attempting to add gdrive changes

--- packages/scratch-vm/src/virtual-machine.js
+++ packages/scratch-vm/src/virtual-machine.js
@@ -29,8 +29,8 @@ require('canvas-toBlob');
 const RESERVED_NAMES = ['_mouse_', '_stage_', '_edge_', '_myself_', '_random_'];
 
 const CORE_EXTENSIONS = [
-    // 'teachableMachine',
-    // 'posenet',
+    'teachableMachine',
+    'textClassification'
     // 'motion',
     // 'looks',
     // 'sound',
@@ -44,26 +44,23 @@ const CORE_EXTENSIONS = [
 
 class ScratchCanvasRecorder {
     constructor(canvas) {
-        this.canvas = canvas;
-    }
-
-    startCapturing() {
         this.mediaSource = new MediaSource();
         this.mediaSource.addEventListener('sourceopen', this.handleSourceOpen.bind(this), false);
-        this.browserMediaRecorder = undefined;
+        this.mediaRecorder = undefined;
         this.recordedBlobs = undefined;
         this.sourceBuffer = undefined;
         this.recording = false;
+        this.canvas = canvas;
         this.video = document.createElement('video');
-        this.video.width = 500;
-        this.video.height = 500;
+        this.video.width=500;
+        this.video.height=500;
         this.video.style.pointerEvents = 'none';
         this.video.style.position = 'fixed';
         this.video.style.top = '0';
         this.video.style.left = '0';
         this.video.style.opacity = '0';
         document.body.appendChild(this.video);
-        this.stream = this.canvas.captureStream(); // frames per second
+        this.stream = canvas.captureStream(); // frames per second
         console.log('Started stream capture from canvas element: ', this.stream);
     }
 
@@ -85,24 +82,34 @@ class ScratchCanvasRecorder {
         this.video.src = window.URL.createObjectURL(superBuffer);
     }
 
+    toggleRecording() {
+        if (recordButton.textContent === 'Start Recording') {
+            startRecording();
+        } else {
+            stopRecording();
+            recordButton.textContent = 'Start Recording';
+            playButton.disabled = false;
+            downloadButton.disabled = false;
+        }
+    }
+
     // The nested try blocks will be simplified when Chrome 47 moves to Stable
     startRecording() {
-        this.startCapturing();
         this.recording = true;
         let options = {mimeType: 'video/webm'};
         this.recordedBlobs = [];
         try {
-            this.browserMediaRecorder = new MediaRecorder(this.stream, options);
+            this.mediaRecorder = new MediaRecorder(this.stream, options);
         } catch (e0) {
             console.log('Unable to create MediaRecorder with options Object: ', e0);
             try {
                 options = {mimeType: 'video/webm,codecs=vp9'};
-                this.browserMediaRecorder = new MediaRecorder(this.stream, options);
+                this.mediaRecorder = new MediaRecorder(this.stream, options);
             } catch (e1) {
                 console.log('Unable to create MediaRecorder with options Object: ', e1);
                 try {
                     options = 'video/vp8'; // Chrome 47
-                    this.browserMediaRecorder = new MediaRecorder(this.stream, options);
+                    this.mediaRecorder = new MediaRecorder(this.stream, options);
                 } catch (e2) {
                     alert('MediaRecorder is not supported by this browser.\n\n' +
                         'Try Firefox 29 or later, or Chrome 47 or later, ' +
@@ -112,23 +119,22 @@ class ScratchCanvasRecorder {
                 }
             }
         }
-        console.log('Created MediaRecorder', this.browserMediaRecorder, 'with options', options);
+        console.log('Created MediaRecorder', this.mediaRecorder, 'with options', options);
         // TODO: Toggle turn off state
         // recordButton.textContent = 'Stop Recording';
         // playButton.disabled = true;
         // downloadButton.disabled = true;
-        this.browserMediaRecorder.onstop = this.handleStop.bind(this);
-        this.browserMediaRecorder.ondataavailable = this.handleDataAvailable.bind(this);
-        this.browserMediaRecorder.start(100); // collect 100ms of data
-        console.log('MediaRecorder started', this.browserMediaRecorder);
+        this.mediaRecorder.onstop = this.handleStop.bind(this);
+        this.mediaRecorder.ondataavailable = this.handleDataAvailable.bind(this);
+        this.mediaRecorder.start(100); // collect 100ms of data
+        console.log('MediaRecorder started', this.mediaRecorder);
     }
 
     stopRecording() {
-        this.browserMediaRecorder.stop();
+        this.mediaRecorder.stop();
         console.log('Recorded Blobs: ', this.recordedBlobs);
         this.video.controls = true;
         this.recording = false;
-
     }
 
     play() {
@@ -593,10 +599,68 @@ class VirtualMachine extends EventEmitter {
         });
     }
 
+    uploadProjectToURL(url) {
+        // get authToken using regex
+        const delimiter = url.indexOf(";");
+        const authToken = url.substr(delimiter+1);
+        url = url.substr(0, delimiter);
+        
+        this.saveProjectSb3().then(content => {
+            nets({
+                url: url,
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': content.type,
+                    'Authorization': 'Bearer ' + authToken,
+                },
+                encoding: undefined,
+                body: content
+            },(err, resp, body) => {
+                if (err) {
+                    console.log(err);
+                    return;
+                }
+                // resp.statusCode
+            })
+        });
+    }
+
     downloadProjectFromURLDirect(url) {
+        // Handle loading google drive files
+        if (url.includes("googleapis.com")) {
+            // get authToken using regex
+            const delimiter = url.indexOf(";");
+            const authToken = url.substr(delimiter+1);
+            url = url.substr(0, delimiter);
+            return new Promise((resolve, reject) => {
+                nets({
+                    url: url,
+                    headers: {
+                        'Authorization': 'Bearer ' + authToken,
+                    },
+                }, (err, resp, body) => {
+                    resolve(this.loadProject(body));
+                })
+            })
+        } else if (url.includes("dropbox.com")) {        
+            // Handle loading dropbox links
+            const dropboxRegex = /\/s\/[A-Za-z0-9]+\/.*.sb3/;
+            const found = url.match(dropboxRegex);
+            if (found.length > 0) url = 'https://dl.dropboxusercontent.com' + found[0];
+        }
+
         return new Promise((resolve, reject) => {
             nets({ url: url }, (err, resp, body) => {
-                resolve(this.loadProject(body));
+                if (err) {
+                    console.log(err);
+                    resolve();
+                }
+
+                if (resp.statusCode !== 200) {
+                    console.log(resp.statusCode);
+                    resolve();
+                } else 
+                    resolve(this.loadProject(body));
             })
         })
     }
