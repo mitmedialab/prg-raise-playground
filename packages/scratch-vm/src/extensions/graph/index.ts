90e3e31da4713f23423d9b5950f8f651db2acab9
Dolev Artzi <dartzi@andrew.cmu.edu>
2022-08-09 19:04:00 -0400

    clear edge graph rather than shadow, basic add/remove opcodes all seem to work

--- packages/scratch-vm/src/extensions/graph/index.ts
+++ packages/scratch-vm/src/extensions/graph/index.ts
@@ -3,6 +3,7 @@ import { Extension } from "../../typescript-support/Extension";
 import { Block } from "../../typescript-support/types";
 import { Graph, vertex, edge } from './graph';
 import { Draw } from './draw';
+import { NumTupSet } from './numtuple';
 
 type DisplayDetails = {
   title: "Basic Graph Theory",
@@ -14,7 +15,7 @@ type DisplayDetails = {
 type Blocks = {
   // playNote: (a: number) => void;
   report: () => number;
-  addVertex: () => void;
+  addVertex: (v:vertex) => void;
   addEdge: (v1:vertex,v2:vertex) => void;
   removeVertex: (v:vertex) => void;
   removeEdge: (v1:vertex,v2:vertex) => void;
@@ -32,13 +33,18 @@ class GraphExtension extends Extension<DisplayDetails, Blocks> {
   options: number[];
   name = () => "ScratchGraph";
   d : Draw;
-  // runtime
-  // runtime;
-  // private blockUtility;
   private vertexDisplay: Map<number,VertexDisplayInfo>;
   private currVertices: Set<vertex>;
-  private currEdges: Set<edge>;
+  private currEdges: Set<string>; //do not reassign currEdges
   private G: Graph;
+  private toStr : (e:edge) => string;
+  private fromStr : (str:string) => edge;
+  private add: (e:edge) => void;
+  private has: (e:edge) => boolean;
+  private remove: (e:edge) => boolean;
+  private values: () => Set<edge>;
+  private forEach: (f:((e:edge) => any)) => void;
+  private range = {min:0,max:19};
 
 
   init() { 
@@ -61,15 +67,23 @@ class GraphExtension extends Extension<DisplayDetails, Blocks> {
     console.log(this.runtime);
 
     this.G = new Graph(20,false);
+
+    this.toStr = NumTupSet.toStr;
+    this.fromStr = NumTupSet.fromStr;
+    this.add = NumTupSet.add(this.currEdges);
+    this.has = NumTupSet.has(this.currEdges);
+    this.remove = NumTupSet.delete_(this.currEdges);
+    this.values = () => NumTupSet.values(this.currEdges);
+    this.forEach = NumTupSet.forEach(this.currEdges);
   }
 
   blockBuilders() {
     return ({
-    'addVertex': (self: GraphExtension): Block<() => void> => {
+    'addVertex': (self: GraphExtension): Block<(v:vertex) => void> => {
       return {
         type: BlockType.Command,
-        args: [],
-        text: () => 'add new vertex',
+        args: [ {type: ArgumentType.Number, defaultValue: 0}],
+        text: (v:vertex) => `add vertex ${v}`,
         operation: this.addVertex.bind(self)
       }
     },
@@ -105,21 +119,34 @@ class GraphExtension extends Extension<DisplayDetails, Blocks> {
 
   })};
 
-  addVertex() {
-    const len = this.currVertices.size;
-    if (len < this.G.max_size) {
-      this.G.addVertex(len);
-      this.currVertices.add(len);
+  private inRange(v:vertex) : boolean {
+    return v >= this.range.min && v <= this.range.max;
+  }
+
+
+
+  addVertex(v: vertex) {
+    if (!(this.inRange(v))) {
+      alert(`vertex values in the range ${this.range.min}-${this.range.max}, inclusive, are accepted`);
     } else {
-      alert(`Max graph size is ${this.G.max_size}`);
+      const len = this.currVertices.size;
+      if (len < this.G.max_size) {
+        this.G.addVertex(v);
+        this.currVertices.add(v);
+      } else {
+        alert(`Max graph size is ${this.G.max_size}`);
+      }
     }
     this.print();
 
   }
 
   addEdge(v1:vertex,v2:vertex) {
-    if (this.G.addEdge([v1,v2])) {
-      this.currEdges.add([v1,v2]);
+    if (!(this.inRange(v1) && this.inRange(v2))) {
+      alert(`vertex values in the range ${this.range.min}-${this.range.max}, inclusive, are accepted`);
+    } else if (this.G.addEdge([v1,v2]) && !this.has([v2,v1])) {
+      console.log('here');
+      this.add([v1,v2]);
       this.currVertices.add(v1);
       this.currVertices.add(v2);
     }
@@ -129,8 +156,10 @@ class GraphExtension extends Extension<DisplayDetails, Blocks> {
 
   removeEdge(v1:vertex,v2:vertex) {
     if (this.G.removeEdge([v1,v2])) {
-      if (!this.currEdges.delete([v1,v2])) {
-        this.currEdges.delete([v2,v1]);
+      console.log('a');
+      if (!this.remove([v1,v2])) {
+        console.log('b');
+        console.log(this.remove([v2,v1]));
       }
     }
     this.print();
@@ -146,9 +175,9 @@ class GraphExtension extends Extension<DisplayDetails, Blocks> {
   removeVertex(v : vertex) {
     if (this.G.removeVertex(v)) {
       this.currVertices.delete(v);
-      const newEdges = Array.from(this.currEdges.values()).filter(([v1,v2]) => v !== v1 && v !== v2);
-      this.currEdges = new Set();
-      newEdges.forEach(e => this.currEdges.add(e));
+      const newEdges = Array.from(this.values()).filter(([v1,v2]) => {console.log([v1,v2],v); return v !== v1 && v !== v2});
+      this.currEdges.clear();
+      newEdges.forEach(e => this.add(e));
       // let foo = Array.from(this.currVertices.values());
     }
     this.print();
