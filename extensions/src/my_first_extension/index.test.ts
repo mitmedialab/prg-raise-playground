a547af8c1a30c8061df9cdb1f4b2b7de6ea86f01
Jimmy Kuhlman <117794650+magicshadow797@users.noreply.github.com>
2023-01-19 15:25:08 -0800

    Delete extensions/src/my_first_extension directory

--- extensions/src/my_first_extension/index.test.ts
+++
@@ -1,9 +0,0 @@
-import { createTestSuite } from "$testing";
-import Extension from '.';
-
-createTestSuite({ Extension, __dirname },
-  {
-    unitTests: undefined,
-    integrationTests: undefined
-  }
-);
