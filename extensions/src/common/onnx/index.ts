6be2749de75b35425b6d4f888b3404cdf501b30e
p-buddy <pmalacho@usc.edu>
2023-01-31 10:47:22 -0500

    Some more refactoring. Need to test again

--- extensions/src/common/onnx/index.ts
+++ extensions/src/common/onnx/index.ts
@@ -4,18 +4,22 @@ import * as ort from "onnxruntime-web";
 type OnWindow = { ort: typeof ort };
 
 export class OnnxRuntime {
-  static CDN = "https://cdn.jsdelivr.net/npm/onnxruntime-web/dist/ort.min.js";
+  static FromCDN = "https://cdn.jsdelivr.net/npm/onnxruntime-web/dist/ort.min.js";
 
   ready = false;
   private onnx: typeof ort;
 
   get runtime(): Promise<typeof ort> { return untilReady(this).then(() => this.onnx); }
 
+  private get globalOnnx() { return (window as any as OnWindow).ort }
+
   constructor() {
-    loadExternalScript(OnnxRuntime.CDN, () => {
-      this.ready = true;
-      this.onnx = (window as any as OnWindow).ort;
-    });
+    const onnx = this.globalOnnx;
+    onnx ? this.resolve(onnx) : loadExternalScript(OnnxRuntime.FromCDN, this.resolve);
   }
 
+  private resolve(onnx?: typeof ort) {
+    this.ready = true;
+    this.onnx = onnx ?? this.globalOnnx;
+  }
 }
