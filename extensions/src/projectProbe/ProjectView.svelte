81957aa80e28c9dec69528721eb65ac14c2d0286
p-buddy <pmalacho@usc.edu>
2023-03-10 00:00:06 -0500

    Got files from other branc and re-inited

---
+++ extensions/src/projectProbe/ProjectView.svelte
@@ -0,0 +1,90 @@
+<script lang="ts">
+import Extension from ".";
+import {activeClass} from "$common";
+import Snippet from "$common/components/Snippet.svelte";
+
+export let extension: Extension;
+
+const container = activeClass;
+let code = extension.projectJson;
+
+$: lines = code.split("\n");
+
+let find: string;
+let replace: string;
+
+$: matches = find ? lines.map(line => Array.from(line.matchAll(new RegExp(find, "g")))).flat() : undefined;
+$: matchLines =  find ? lines.filter(line => Array.from(line.matchAll(new RegExp(find, "g"))).length > 0): undefined;
+$: numMatches = matches?.length;
+
+let editable = false;
+
+</script>
+
+<style>
+.container {
+  padding: 30px;
+  height: 80vh;
+  width: 80vw;
+  overflow: scroll;
+}
+.matches {
+  background-color: white;
+}
+
+.find {
+  background-color: yellow;
+  display: inline-block;
+}
+
+.replace {
+  background-color: rgb(168, 80, 186);
+  font-style: italic;
+  display: inline-block;
+}
+</style>
+
+<div class:container>
+  <div>
+    Find: <input bind:value={find} /> 
+    Replace: <input bind:value={replace} /> 
+    <button on:click={() => code = lines.map(line => line.replaceAll(new RegExp(find, "g"), replace)).join("\n")}>execute</button>
+  </div>
+  <div>
+    <i>{numMatches} matches</i>
+  </div>
+  <div class="matches">
+    {#if matches}
+      {#each matchLines as match}
+        <div>
+          {#if replace}
+            {#each match.split(find) as segment, index}
+                {segment}{#if index < match.split(find).length - 1}<div class="replace">{replace}</div>{/if}
+            {/each}
+          {:else}
+            {#each match.split(find) as segment, index}
+              {segment}{#if index < match.split(find).length - 1}<div class="find">{find}</div>{/if}
+            {/each}
+          {/if}
+        </div>
+      {/each}
+    {/if}
+  </div>
+  <div>
+  <center>
+    <button on:click={() => extension.downloadUpdatedProjectJson(code)}>
+      Download Updated Project
+    </button>
+  </center>
+  </div>
+  Editable {editable}
+  <label class="switch">
+    <input type="checkbox" bind:checked={editable}>
+    <span class="slider round"></span>
+  </label>
+  {#if editable}
+    <textarea bind:value={code}></textarea>
+  {:else}
+    <Snippet {code} copyable={"top"}/>
+  {/if}
+</div>
