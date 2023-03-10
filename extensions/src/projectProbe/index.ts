958a77b275235325b82b7fdad4d27abf9bfa8b8e
p-buddy <pmalacho@usc.edu>
2023-03-09 19:57:10 -0500

    implemented project probe to download updated files

--- extensions/src/projectProbe/index.ts
+++ extensions/src/projectProbe/index.ts
@@ -1,5 +1,6 @@
 import { Environment, extension, ExtensionMenuDisplayDetails, block } from "$common";
 import { unzip } from 'unzipit';
+import JSZip from "jszip";
 
 const details: ExtensionMenuDisplayDetails = {
   name: "Project Probe",
@@ -7,14 +8,15 @@ const details: ExtensionMenuDisplayDetails = {
 };
 
 export default class _ extends extension(details, "ui") {
-  json: string;
+  projectJson: string;
+  currentEntries: Awaited<ReturnType<typeof unzip>>["entries"];
 
   init(env: Environment): void { }
 
   @block({
     type: "command",
     arg: "string",
-    text: (url) => `Download project from ${url} and probe`
+    text: (url) => `Probe & modify project JSON from ${url}`
   })
   async probeProject(url: string) {
 
@@ -27,7 +29,49 @@ export default class _ extends extension(details, "ui") {
     const { entries } = await unzip(url);
     const project = entries["project.json"];
     const x = await project.json();
-    this.json = JSON.stringify(x, null, 3);
+    this.currentEntries = entries;
+    this.projectJson = JSON.stringify(x, null, 3);
     this.openUI("JsonView");
   }
+
+  async downloadUpdatedProjectJson(projectJson: string) {
+    const zip = new JSZip();
+    zip.file('project.json', projectJson);
+
+    for (const key in this.currentEntries) {
+      if (key === "project.json") continue;
+      const blob = await this.currentEntries[key].blob();
+      zip.file(key, blob)
+    }
+
+    const final = await zip.generateAsync({
+      type: 'blob',
+      mimeType: 'application/x.scratch.sb3',
+      compression: 'DEFLATE',
+      compressionOptions: {
+        level: 6 // Tradeoff between best speed (1) and best compression (9)
+      }
+    });
+
+    saveBlob("Update.sb3", final);
+  }
 }
+
+const saveBlob = (filename, blob) => {
+  const downloadLink = document.createElement('a');
+  document.body.appendChild(downloadLink);
+
+  // Use special ms version if available to get it working on Edge.
+  if (navigator["msSaveOrOpenBlob"]) {
+    navigator["msSaveOrOpenBlob"](blob, filename);
+    return;
+  }
+
+  const url = window.URL.createObjectURL(blob);
+  downloadLink.href = url;
+  downloadLink.download = filename;
+  downloadLink.type = blob.type;
+  downloadLink.click();
+  window.URL.revokeObjectURL(url);
+  document.body.removeChild(downloadLink);
+};
