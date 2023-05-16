fcb8d727598fb002dd6e79ebcaaabede8143a38a
Parker Malachowsky <pmalacho@media.mit.edu>
2023-05-16 16:44:42 -0400

    completed install

--- packages/scratch-gui/src/svelte/Modal.svelte
+++ packages/scratch-gui/src/svelte/Modal.svelte
@@ -1,49 +1,58 @@
 <script lang="ts" context="module">
-  import type _ExtensionManager from 'scratch-vm/src/extension-support/extension-manager';
-  import type { Extension } from "../../../../extensions/src/common/Extension";
-  import type _VirtualMachine from "scratch-vm/src/virtual-machine";
+    import type { ExtensionInstance } from "../../../../extensions/src/common/Extension";
 
-  type ExtensionID = string;
-  type ComponentName = string;
+    type ExtensionID = string;
+    type ComponentName = string;
 
-  type Payload = {
-    target: HTMLDivElement;
-    props: { close: () => void, extension: Extension<any, any> };
-  }
+    type Payload = {
+        target: HTMLDivElement;
+        props: { close: () => void; extension: ExtensionInstance };
+    };
 
-  type UIConstructor = (details: Payload) => void; 
+    type UIConstructor = (details: Payload) => void;
 
-  type ExtensionManager = _ExtensionManager & { 
-    getAuxiliaryObject: (id: ExtensionID, name: ComponentName) => UIConstructor,
-    getExtensionInstance: (id: ExtensionID) => Extension<any, any>
-  };
-  type VirtualMachine = _VirtualMachine & { extensionManager: ExtensionManager };
+    type ExtensionManager = {
+        getAuxiliaryObject: (
+            id: ExtensionID,
+            name: ComponentName
+        ) => UIConstructor;
+        getExtensionInstance: (id: ExtensionID) => ExtensionInstance;
+    };
+
+    type VirtualMachine = {
+        extensionManager: ExtensionManager;
+    };
 </script>
 
 <script lang="ts">
-  import { onMount, onDestroy } from 'svelte';
+    import { onMount, onDestroy } from "svelte";
 
-  // svelte-ignore unused-export-let
-  export let id: ExtensionID;
-  // svelte-ignore unused-export-let
-  export let component: ComponentName;
-  // svelte-ignore unused-export-let
-  export let name: string;
-  // svelte-ignore unused-export-let
-  export let vm: VirtualMachine;
-  // svelte-ignore unused-export-let
-  export let close: () => void;
+    // svelte-ignore unused-export-let
+    export let id: ExtensionID;
+    // svelte-ignore unused-export-let
+    export let component: ComponentName;
+    // svelte-ignore unused-export-let
+    export let name: string;
+    // svelte-ignore unused-export-let
+    export let vm: VirtualMachine;
+    // svelte-ignore unused-export-let
+    export let close: () => void;
 
-  let target: HTMLDivElement;
+    let target: HTMLDivElement;
 
-  onMount(async () => {
-    const props = { close, extension: vm.extensionManager.getExtensionInstance(id) };
-    const options = { target, props };
-    const constructor = vm.extensionManager.getAuxiliaryObject(id, component);
-    new constructor(options);
-    return;
-  })
-
+    onMount(async () => {
+        const props = {
+            close,
+            extension: vm.extensionManager.getExtensionInstance(id),
+        };
+        const options = { target, props };
+        const constructor = vm.extensionManager.getAuxiliaryObject(
+            id,
+            component
+        );
+        new constructor(options);
+        return;
+    });
 </script>
 
 <div bind:this={target} />
