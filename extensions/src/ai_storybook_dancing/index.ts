bef58709d93f6b1596e09e3c24b5dda39f255700
Jimmy Kuhlman <jkuhl2022@gmail.com>
2023-06-06 18:08:49 -0700

    Added inline images and started on zoomed-in workspace

--- extensions/src/ai_storybook_dancing/index.ts
+++ extensions/src/ai_storybook_dancing/index.ts
@@ -1,7 +1,11 @@
-import { Environment, ExtensionMenuDisplayDetails, extension, block, SaveDataHandler, RuntimeEvent, } from "$common";
+import { Environment, ExtensionMenuDisplayDetails, extension, block, SaveDataHandler, RuntimeEvent, ArgumentType, } from "$common";
 import BlockUtility from "$root/packages/scratch-vm/src/engine/block-utility";
 import { hideNonBlocklyElements, stretchWorkspaceToScreen } from "./layout";
 import { announce, requestDanceMove, untilMessageReceived, type DanceMove } from "./messaging";
+import hop from "./inlineImages/hop.png";
+import stepLeft from "./inlineImages/left.png";
+import stepRight from "./inlineImages/right.png";
+import spin from "./inlineImages/spin-left.png"; // TODO: Change to a generic spin image
 
 const details: ExtensionMenuDisplayDetails = { name: "Dancing Activity for AI Storybook" };
 
@@ -13,9 +17,22 @@ const dance = async (move: DanceMove) => {
 let flipFlopper = false;
 
 export default class AiStorybookDancing extends extension(details, "blockly", "customSaveData") {
+  
   async init(env: Environment) {
     hideNonBlocklyElements();
     stretchWorkspaceToScreen();
+
+    const workspace = this.blockly.getMainWorkspace();
+    // workspace.options.zoomOptions.startScale = 1.2;
+
+    // const w = this.blockly.Mutator.prototype.getWorkspace();
+    // console.log(w);
+    // const rect = w.zoomControls_.getBoundingRectangle();
+    // w.zoom(50, 50, 3);
+
+    // workspace.options.zoomOptions.minScale = 1.2;
+    // workspace.options.zoomOptions.maxScale = 1.2;
+
     announce("ready");
   }
 
@@ -36,38 +53,60 @@ export default class AiStorybookDancing extends extension(details, "blockly", "c
   });
 
   @block({
-    text: "Hop",
+    text: (hop) => `${hop}`,
+    arg: {
+      type: "image",
+      uri: hop,
+      alt: "Hop"
+    },
     type: "command"
   })
-  async hop(util: BlockUtility) {
+  async hop(hop: "inline image") {
     await dance("hop");
   }
 
   @block({
-    text: "Step left",
+    text: (stepLeft) => `${stepLeft}`,
+    arg: {
+      type: "image",
+      uri: stepLeft,
+      alt: "Step left"
+    },
     type: "command"
   })
-  async stepLeft() {
+  async stepLeft(stepLeft: "inline image") {
     await dance("swivel left");
   }
 
   @block({
-    text: "Step right",
+    text: (stepRight) => `${stepRight}`,
+    arg: {
+      type: "image",
+      uri: stepRight,
+      alt: "Step right"
+    },
     type: "command"
   })
-  async stepRight() {
+  async stepRight(stepRight: "inline image") {
     await dance("swivel right");
   }
 
   @block({
-    text: (direction: 'left' | 'right') => `Spin ${direction}`,
+    text: (spin, direction: 'left' | 'right') => `${spin} ${direction}`,
     type: "command",
-    arg: {
-      type: 'string',
-      options: ['left', 'right']
-    }
+    args: [{
+      type: "image",
+      uri: spin,
+      alt: "Spin"
+    },
+    {
+      type: "custom",
+      options:
+        [{ text: "↻", value: "left" }, // TODO: Find better symbols for spinning
+        { text: "↺", value: "right" }]
+    }],
   })
-  async spin(direction: 'left' | 'right') {
+  async spin(spin: "inline image", direction: 'left' | 'right') {
     await dance(`spin ${direction}`);
   }
 
