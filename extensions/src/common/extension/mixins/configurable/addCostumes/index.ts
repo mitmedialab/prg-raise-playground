609ef536b171164c95bd8c8dcc0d36c9aff95f0d
randi-c-dubs <randi.w.2012@gmail.com>
2023-08-20 01:35:26 +0000

    Adding additional helper functions to addCostumes

--- extensions/src/common/extension/mixins/configurable/addCostumes/index.ts
+++ extensions/src/common/extension/mixins/configurable/addCostumes/index.ts
@@ -53,6 +53,59 @@ export default function <T extends MinimalExtensionConstructor>(Ctor: T) {
       if (action === "add and set") target.setCostume(length);
     }
 
+    /**
+     * Add a costume to the current sprite based on a bitmpa input
+     * @param {Target} target (e.g. `util.target`)
+     * @param {string} bitmapImage What image to use to create the costume
+     * @param {"add only" | "add and set"} action What action should be applied
+     * - **_add only_**: generates the costume and append it it to the sprite's costume library
+     * - **_add and set_**: Both generate the costume (adding it to the sprite's costume library) and set it as the sprite's current costume
+     * @param {string?} name optional name to attach to the costume
+     */
+    async addCostumeBitmap(target: Target, bitmapImage: string, action: "add only" | "add and set", name?: string) {
+      if (!isRenderedTarget(target)) return console.warn("Costume could not be added as the supplied target wasn't a rendered target");
+
+      name ??= `virtualJibo_generated_${Date.now()}`;
+      bitmapAdapter ??= new MockBitmapAdapter();
+      //urlHelper ??= getUrlHelper(image);
+
+      // storage is of type: https://github.com/LLK/scratch-storage/blob/develop/src/ScratchStorage.js
+      const { storage } = this.runtime;
+      const dataFormat = storage.DataFormat.PNG;
+      const assetType = storage.AssetType.ImageBitmap;
+      const dataBuffer = await bitmapAdapter.importBitmap(bitmapImage);
+
+      const asset = storage.createAsset(assetType, dataFormat, dataBuffer, null, true);
+      const { assetId } = asset;
+      const costume = { name, dataFormat, asset, md5: `${assetId}.${dataFormat}`, assetId };
+
+      await this.runtime.addCostume(costume);
+
+      const { length } = target.getCostumes();
+
+      target.addCostume(costume, length);
+      if (action === "add and set") target.setCostume(length);
+    }
+
+    /**
+     * Add a costume to the current sprite based on same image data
+     * @param {Target} target (e.g. `util.target`)
+     * @param {string?} name costume name to look for
+     */
+    setCostumeByName(target: Target, name: string): boolean {
+      if (!isRenderedTarget(target)) {
+        console.warn("Costume could not be set as the supplied target wasn't a rendered target");
+        return false;
+      }
+
+      let costumeIdx = target.getCostumeIndexByName(name);
+      if (costumeIdx >= 0) {
+        target.setCostume(costumeIdx);
+        return true;
+      }
+      return false;
+    }
+
   }
 
   return ExtensionWithCustomSupport;
