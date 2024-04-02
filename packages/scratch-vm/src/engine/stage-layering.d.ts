09d76c72b9ede34e35898c88faef42851d0978e3
Github Action (authored by pmalacho-mit) <pmalacho-mit@users.noreply.github.com>
2024-04-01 18:05:21 -0700

    deleting packages folder

--- packages/scratch-vm/src/engine/stage-layering.d.ts
+++
@@ -1,8 +0,0 @@
-export = StageLayering;
-declare class StageLayering {
-    static get BACKGROUND_LAYER(): string;
-    static get VIDEO_LAYER(): string;
-    static get PEN_LAYER(): string;
-    static get SPRITE_LAYER(): string;
-    static get LAYER_GROUPS(): string[];
-}
--- packages/scratch-vm/src/extension-support/prg/bundle-loader.d.ts
+++
@@ -1,3 +0,0 @@
-export function tryInitExtension(extension: Extension): Promise<any>;
-export function tryGetExtensionConstructorFromBundle(id: string): Constructor<Extension>;
-export function tryGetAuxiliaryObjectFromLoadedBundle(id: any, name: any): any;
