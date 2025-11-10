1eee5fa35a1ab14dbdac2f8c8970d8332ecd59ac
Maya Rajan <mayarajan222@gmail.com>
2025-11-10 13:54:28 -0500

    no gpu

--- extensions/src/poseHand/index.ts
+++ extensions/src/poseHand/index.ts
@@ -162,7 +162,6 @@ export default class PoseHand extends Extension<Details, Blocks> {
       {
         baseOptions: {
           modelAssetPath: "https://storage.googleapis.com/mediapipe-models/hand_landmarker/hand_landmarker/float16/latest/hand_landmarker.task",
-          delegate: "GPU",
         },
         runningMode: "VIDEO",
         numHands: 2
