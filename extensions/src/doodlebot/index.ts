534e06e27b9c2bf6a974ba67922be1cf4fb817e0
Maya Rajan <mayarajan222@gmail.com>
2026-06-15 16:17:49 -0400

    new blocks

--- extensions/src/doodlebot/index.ts
+++ extensions/src/doodlebot/index.ts
@@ -250,6 +250,24 @@ export default class DoodlebotBlocks extends extension(details, "ui", "customArg
     this.openUI("Connect");
   }
 
+  @block({
+    type: "command",
+    text: (angle: number) => `set pen down angle to ${angle} degrees`,
+    arg: { type: 'number', defaultValue: 40 },
+  })
+  async penDownAngle(angle: number) {
+    await this.doodlebot.sendBLECommand("i", angle);
+  }
+
+  @block({
+    type: "command",
+    text: (angle: number) => `set pen up angle to ${angle} degrees`,
+    arg: { type: 'number', defaultValue: 1 },
+  })
+  async penUpAngle(angle: number) {
+    await this.doodlebot.sendBLECommand("j", angle);
+  }
+
 
   @block({
     type: "command",
